2e2b4855a1bf1c0886cce3f7a1aa3b3795456691
comep <comep@PC-CME>
2024-10-11 18:06:25 +0200

    Ajoutez des fichiers projet.

---
+++ Générateur de Mots de passe/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Générateur_de_Mots_de_passe
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            bool retry = true;
+            while (retry)
+            {
+                // Réglage de la console
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                // Implémentation de l'intégralité des caractères
+                string lowercase = "abcdefghijklmnopqrstuvwxyz";
+                string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                string digits = "1234567890";
+                string symbols = "!@#$%^&*()-_=+[{]}|;:',<.>/?`~";
+                // Combiner tous les caractères ensembles
+                string allCharacters = lowercase + uppercase + digits + symbols;
+                // Création d'un tableau avec l'intégralité des caractères.
+                char[] characterArray = allCharacters.ToCharArray();
+                // Génération du mot de passe
+                Console.Write("Combien souhaitez-vous de caractères ? ");
+                int pwLength;
+                while (!int.TryParse(Console.ReadLine(), out pwLength) || pwLength <= 0)
+                {
+                    Console.WriteLine("Entrée invalide. Veuillez rentrer un entier positif.");
+                }
+                string password = GeneratePassword(pwLength, characterArray);
+                // Ajout du mot de passe dans le presse-papier, et affichage de celui-ci en rouge foncé
+                Clipboard.SetText(password);
+                Console.WriteLine("Votre mot de passe est a été ajouté au presse-papier. Le voici :");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(password);
+                Console.ReadLine();
+            }
+        }
+        static string GeneratePassword(int length, char[] characterArray)
+        {
+            Random random = new Random();
+            char[] password = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                int RandomIndex = random.Next(characterArray.Length);
+                password[i] = characterArray[RandomIndex];
+            }
+            return new string(password);
+        }
+    }
+}
---
+++ Générateur de Mots de passe/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Générateur de Mots de passe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Générateur de Mots de passe")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("6a428ddb-16c8-4dff-bedd-3d3c8cf44a79")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
